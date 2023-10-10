ce9daab7a419f0e3eae05d01ca5606270b6fff68
DoctorKeemee <premek@man-cz.eu>
2023-10-10 19:51:53 +0200

    Přidání hlavní stránky.

---
+++ src/src/components/Learning.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="columns">
+    <div class="column">
+      First column
+    </div>
+    <div class="column">
+      Second column
+    </div>
+    <div class="column">
+      Third column
+    </div>
+    <div class="column">
+      Fourth column
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from "vue";
+  export default defineComponent({
+    name: "Learning"
+  })
+</script>
+
+
+
+<style scoped>
+
+</style>
