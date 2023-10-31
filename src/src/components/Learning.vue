f11b542e5828e52430511e644dfcda6d1052db28
DoctorKeemee <premek@man-cz.eu>
2023-10-31 20:03:01 +0100

    Stylování

--- src/src/components/Learning.vue
+++ src/src/components/Learning.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="word-card" >
+  <nav class="menu">
+  <button @click="chooseFile" class="button darkorange">Import from XLSX</button>
+  <button @click="exportToCSV" class="button red_button" > Export to XLSX</button>
+  </nav>
+  <div class="container" >
+
+    <div class="word-card" >
     <div class="word" id="word">{{ currentWord }}</div>
     <div class="definition" id="definition">{{ currentDefinition }}</div>
   </div>
-  <button @click="showNextWord" style="background: green">Chápu to!</button>
-
-  <button @click="chooseFile" style="background: darkorange">Import from XLSX</button>
-  <button @click="exportToCSV" style="background: darkred">Export to XLSX</button>
-  <input type="file" ref="fileInput" style="opacity: 0;" @change="importFromCSV" accept=".csv">
-
+  <button @click="showNextWord" class="button green_button">Chápu to!</button>
+<input type="file" ref="fileInput" style="opacity: 0;" @change="importFromCSV" accept=".csv">
+  </div>
 </template>
 
 <script lang="ts">
@@ -22,7 +25,7 @@
           { word: "Ubiquitous", definition: "Existing or being everywhere at the same time", explained: false },
           { word: "Sycophant", definition: "A person who acts obsequiously towards someone important", explained: false  },
           { word: "Ephemeral", definition: "Lasting for a very short time", explained: false  }
-        ],
+        ] as { word: string; definition: string; explained: boolean }[],
         currentIndex: ref(0),
       };
     },
@@ -64,6 +67,16 @@
       },
       parseCSV(csvData: string) {
         console.log(csvData);
+        const lines: string[] = csvData.split('\n');
+        this.wordList = [] as { word: string; definition: string; explained: boolean }[]
+        for (let i = 0; i < lines.length; i++) {
+          const word: string[] = lines[i] .split(';');
+          this.wordList.push({
+            word: word[0],
+            definition: word[1],
+            explained: false,
+          });
+        }
 
       },
       exportToCSV(){
@@ -93,6 +106,25 @@ body {
   height: 100vh;
   margin: 0;
 }
+nav{
+  margin-top: 15px;
+  margin-bottom: 150px;
+}
+.button{
+  color: antiquewhite;
+  font-size: larger;
+  padding: 5px;
+  margin: 5px;
+}
+.green_button {
+  background: green;
+}
+.red_button{
+  background: darkred;
+}
+.darkorange{
+  background: darkorange;
+}
 .word-card {
   border: 1px solid #ccc;
   border-radius: 5px;
@@ -100,6 +132,11 @@ body {
   box-shadow: 2px 2px 5px #aaa;
   width: 300px;
   text-align: center;
+  background: #181717;
+  background-image: url("https://image.api.playstation.com/vulcan/img/cfn/11307x4B5WLoVoIUtdewG4uJ_YuDRTwBxQy0qP8ylgazLLc01PBxbsFG1pGOWmqhZsxnNkrU3GXbdXIowBAstzlrhtQ4LCI4.png");
+  background-repeat: no-repeat;
+  background-position: right top;
+  color: black;
 }
 .word {
   font-size: 36px;
